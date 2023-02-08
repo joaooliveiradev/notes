888ff0d4b323020787f9986ef7625844dac255e2
João Oliveira <joaooliveira1775@gmail.com>
2023-02-08 14:30:38 -0300

    chore: setting workers on jest and set coverage on tests

--- frontend/tests/react-testing-library/jest.config.ts
+++ frontend/tests/react-testing-library/jest.config.ts
@@ -4,6 +4,8 @@ const config: Config.InitialOptions = {
   testEnvironment: "jsdom",
   preset: "ts-jest",
   setupFilesAfterEnv: ["@testing-library/jest-dom/extend-expect"],
+  maxWorkers: "50%",
+  collectCoverage: true,
   transform: {
     "^.+\\.(ts|tsx|js|jsx)$": [
       "ts-jest",
