080165ebb81d3e5a7a65c79311fa0ff73a38d792
João Oliveira <joaooliveira1775@gmail.com>
2023-02-08 10:02:52 -0300

    chore: passing the jest dom module before each test

--- frontend/tests/react-testing-library/jest.config.ts
+++ frontend/tests/react-testing-library/jest.config.ts
@@ -1,7 +1,9 @@
 import type { Config } from "@jest/types";
+
 const config: Config.InitialOptions = {
   testEnvironment: "jsdom",
   preset: "ts-jest",
+  setupFilesAfterEnv: ["@testing-library/jest-dom/extend-expect"],
   transform: {
     "^.+\\.(ts|tsx|js|jsx)$": [
       "ts-jest",
