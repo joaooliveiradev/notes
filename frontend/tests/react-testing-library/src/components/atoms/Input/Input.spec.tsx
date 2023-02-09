8d4549ab309e7448745717f4a6b68da73b8f85e1
João Oliveira <joaooliveira1775@gmail.com>
2023-02-09 14:58:11 -0300

    feat: add tests for input component

--- frontend/tests/react-testing-library/src/components/atoms/Input/Input.spec.tsx
+++ frontend/tests/react-testing-library/src/components/atoms/Input/Input.spec.tsx
@@ -1,12 +1,36 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { InputCustom } from "./index";
 
-const renderSetup = () => render(<InputCustom placeholder="inputExample" />);
+const renderSetup = () => render(<InputCustom />);
 
 describe("<InputCustom />", () => {
   it("should be rendering", () => {
-    renderSetup()
-    const element = screen.getByPlaceholderText("inputExample");
+    renderSetup();
+    const element = screen.getByRole("textbox");
     expect(element).toBeInTheDocument();
+  });
+  it("should allow the value on the input", async () => {
+    renderSetup();
+    const user = userEvent.setup();
+    const element = screen.getByRole("textbox");
+    await user.type(element, "someGithubProfile");
+    expect(element).toHaveValue("someGithubProfile");
+  });
+  it("should render with the correct props", () => {
+    render(
+      <InputCustom placeholder="Github User" name="searchUser" type="text" />
+    );
+    const element = screen.getByRole("textbox");
+    expect(element).toHaveAttribute("placeholder", "Github User");
+    expect(element).toHaveAttribute("name", "searchUser");
+    expect(element).toHaveAttribute("type", "text");
+  });
+  it("should focus correctly on the input", async () => {
+    renderSetup();
+    const user = userEvent.setup();
+    const element = screen.getByRole("textbox");
+    await user.tab();
+    expect(element).toHaveFocus();
   });
 });
