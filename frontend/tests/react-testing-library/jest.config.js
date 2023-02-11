765428a876970757c91c236d8496665daab16764
João Oliveira <joaooliveira1775@gmail.com>
2023-02-10 21:34:39 -0300

    chore: jest config to match path alias

--- frontend/tests/react-testing-library/jest.config.js
+++ frontend/tests/react-testing-library/jest.config.js
@@ -17,4 +17,12 @@ module.exports = {
       },
     ],
   },
+  // this config is to swc jest can parse path alias in tsconfig
+  // $1 means = the first capturing group
+  // $2 means = the second, etc
+  // so basically we match to any path after @/, put in a group and we will refeer to the root/$1, this $1 it's the path that we catch in the group (.*)
+  // good to know this, i need to study more regular expressions hehe.
+  moduleNameMapper: {
+    "@/(.*)": ["<rootDir>/src/$1"],
+  },
 };
