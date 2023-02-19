25db218fa6a764363094647190912f93adc1cca5
João Oliveira <joaooliveira1775@gmail.com>
2023-02-19 15:07:59 -0300

    removing unused config on test utils

--- frontend/tests/react-testing-library/src/lib/test-utils.ts
+++ frontend/tests/react-testing-library/src/lib/test-utils.ts
@@ -2,7 +2,6 @@ import "@testing-library/jest-dom";
 import { useDispatch, useSelector } from "react-redux";
 import store from "@/store/GitHubUser/store";
 import { rest } from "msw";
-import { type UsersRepositoriesResponse } from "@/services/Users/Users.types";
 import { setupServer } from "msw/node";
 
 type AppDispatch = typeof store.dispatch;
@@ -11,34 +10,14 @@ type AppDispatch = typeof store.dispatch;
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 export const useAppSelector = useSelector;
 
-export const repositories: UsersRepositoriesResponse[] = [
-  {
-    id: "c627deb2a-c917-4dcb-ac32-406xc6c6c911d",
-    fork: false,
-    html_url: "https://github.com/Santosl2/github-finder",
-    language: "pt-BR",
-    name: "Github Finder v1",
-    stargazers_count: 1,
-  },
-  {
-    id: "c627debdasdsa-c917-4dcd-ac32-406xc6c6c911d",
-    fork: false,
-    html_url: "https://github.com/Santosl2/github-finder",
-    language: "pt-BR",
-    name: "Github Finder v2",
-    stargazers_count: 1,
-  },
-];
-
 const handlers = [
-  rest.get("/getUser", (_, res, ctx) => {
-    return res(ctx.status(200), ctx.json(repositories));
+  //to fail in DevRepositories
+  rest.get("https://api.github.com/users/:users/repos", (req, res, ctx) => {
+    return res(ctx.status(500));
   }),
 ];
 
 export const server = setupServer(...handlers);
-
-beforeAll(() => server.listen());
 
 afterEach(() => server.resetHandlers());
 
