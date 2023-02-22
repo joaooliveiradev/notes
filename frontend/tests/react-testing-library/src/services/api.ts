f6d7f36ff07f6aed7bb5fb7218b3e2687b5dcd4b
João Oliveira <joaooliveira1775@gmail.com>
2023-02-22 11:24:49 -0300

    feat: put github token in test enviroment too

--- frontend/tests/react-testing-library/src/services/api.ts
+++ frontend/tests/react-testing-library/src/services/api.ts
@@ -1,11 +1,10 @@
+import * as dotenv from "dotenv"
+dotenv.config()
 import axios from "axios";
 
 export const api = axios.create({
   baseURL: "https://api.github.com/users/",
-  headers:
-    process.env.NODE_ENV === "production"
-      ? {
-          Authorization: `Bearer ${process.env.GITHUB_RATE_LIMIT_TOKEN}`,
-        }
-      : undefined,
+  headers: {
+    Authorization: `Bearer ${process.env.GITHUB_RATE_LIMIT_TOKEN}`,
+  },
 });
